97147745dc0ee22708603f2bef1f182e26426d21
Rplus <rplus.tw@gmail.com>
2019-06-16 01:25:59 +0800

    change to use workbox.js for PWA Service Worker

--- sw.js
+++ sw.js
@@ -1,70 +1,67 @@
-var cacheName = 'shiny-list-v1.0';
-var urlsToCache = [
-	"./favicon.png",
-	"./icon.png",
-	"./index.html",
-	"./l10n.js",
-	"./pms.js",
-	"./manifest.json",
-	"./screenshot2.png",
-	"./script.js",
-	"./style.css",
-	"./style.scss"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-self.addEventListener('activate', function(event) {
-  console.log('Finally active. Ready to start serving content!');  
-});
+workbox.setConfig({ debug: location.hostname === 'localhost' });
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
- 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+// html
+workbox.routing.registerRoute(
+  new RegExp('.*\.html$'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'pm-shiny-cache--html'
+  })
+);
 
-        return fetch(event.request);
-      }
-    )
-  );
-  event.waitUntil(
-    updateservicew(event.request)
-	.then(refresh)
-  );
-});
+// vender assets
+workbox.routing.registerRoute(
+  // new RegExp('.\/html2canvas\/'),
+  /.*\/(html2canvas.min.js|polyfill.min.js)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'pm-shiny-cache--vendor'
+  })
+);
 
-function updateservicew(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
+// js
+workbox.routing.registerRoute(
+  /.*\.js$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'pm-shiny-cache--js'
+  })
+);
 
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-		var message = {
-        type: 'refresh',
-        url: response.url,
-		eTag: response.headers.get('ETag')
-      };
-	  client.postMessage(JSON.stringify(message));
-    });
-  });
-}
+// css
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'pm-shiny-cache--css'
+  })
+);
+
+// images
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  // new RegExp('.\/assets\/regular\/.*\.(?:png|jpg|jpeg|svg|gif)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'pm-shiny-cache--image',
+  })
+);
+
+// // cdn images
+// workbox.routing.registerRoute(
+//   /.*weserv.*\.(?:png|jpg|jpeg|svg|gif)$/,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'pm-shiny-cache--cdn-image',
+//     plugins: [
+//       new workbox.cacheableResponse.Plugin({
+//         statuses: [0, 200],
+//       }),
+//       new workbox.expiration.Plugin({
+//         // Cache only 20 images.
+//         maxEntries: 200,
+//         // Cache for a maximum of a month.
+//         maxAgeSeconds: 7 * 24 * 60 * 60 * 30,
+//       })
+//     ],
+//   })
+// );
+
+// precache放routing之後
+workbox.precaching.precacheAndRoute([]);
