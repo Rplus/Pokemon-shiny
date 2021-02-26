6508bffa850e7ebfbf56f160372bf68dfae9d738
Rplus <rplus.tw@gmail.com>
2021-02-27 05:42:49 +0800

    add script to compress and copy json

--- makefile
+++ makefile
@@ -12,7 +12,8 @@ build-sw:
 
 copy-assets:
 	mkdir -p public; \
-	cp -r ./assets/* ./public/
+	cp -r ./assets/* ./public/; \
+	node ./tasks/com-json.js;
 
 updateTime:
 	sed -i -r "s/[12][0-9]{3}-[01][0-9]-[0-3][0-9]/`date '+%F'`/g" './assets/index.html'; \
---
+++ tasks/com-json.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const outputJSON = (json = {}, fileName, jsonSpace = 2) => {
+  let fileContent = JSON.stringify(json, null, jsonSpace);
+  fs.writeFileSync(fileName, fileContent);
+  console.log(`JSON saved as ${fileName}! ( ${fileContent.length / 1000} kb )`);
+};
+
+let pms = fs.readFileSync('./assets/pms.json', 'utf8');
+let names = fs.readFileSync('./assets/name.json', 'utf8');
+
+names = JSON.parse(names);
+
+// remove dup & use 'en' fallback
+for (var dex in names) {
+  names[dex]
+  for (var lang in names[dex]) {
+    if (
+      lang === 'Romaji' ||
+      (lang !== 'en' && names[dex][lang] === names[dex].en)
+    ) {
+      delete names[dex][lang];
+    }
+  }
+}
+
+outputJSON(JSON.parse(pms), './public/pms.json', 0);
+outputJSON(names, './public/name.json', 0);
